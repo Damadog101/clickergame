44e1874949dd0981b8311c0c68c432fb34ee53e3
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-06 22:43:19 -0700

    g gs fdg sd

--- script/script.js
+++ script/script.js
@@ -1,7 +1,12 @@
 let clickBox = document.getElementById("clickHere");
 let displayScore = document.getElementById("scoreBox");
-let score = 0;
 let coins = `<i class="fa-solid fa-coins gold"></i>`;
+let yoshiButton = document.getElementById("buyYoshi");
+let cpsBox = document.getElementById("cpsBox");
+
+let score = 0;
+let cps = 10;
+
 function playSound(sound) {
 	let soundPlayed = new Audio(sound);
 	soundPlayed.play();
@@ -11,4 +16,22 @@ clickBox.addEventListener("click", () => {
 	score += 1;
 	displayScore.innerHTML = `${score} ${coins}`;
 	playSound("/sound/bonk.mp3");
+});
+
+yoshiButton.addEventListener("click", () => {
+	cps += 10;
+	score -= 20;
+
+	displayScore.innerHTML = `${score} ${coins}`;
+	cpsBox.innerHTML = `${cps} ${coins}`;
+});
+
+window.addEventListener("load", () => {
+	setInterval(() => {
+		if ((cps = 0)) {
+			return;
+		} else {
+			score += cps;
+		}
+	}, 1000);
 });
