bcfb4facb50bcd79eeb33b8fcd9b97c116bbdd78
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-06 22:19:44 -0700

    styling

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 let clickBox = document.getElementById("clickHere");
 let displayScore = document.getElementById("scoreBox");
 let score = 0;
-
+let coins = `<i class="fa-solid fa-coins gold"></i>`;
 function playSound(sound) {
 	let soundPlayed = new Audio(sound);
 	soundPlayed.play();
@@ -9,6 +9,6 @@ function playSound(sound) {
 
 clickBox.addEventListener("click", () => {
 	score += 1;
-	displayScore.innerText = score;
+	displayScore.innerHTML = `${score} ${coins}`;
 	playSound("/sound/bonk.mp3");
 });
