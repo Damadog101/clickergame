41ab7007faec1d8d3ebbef16e4500b340a0713a2
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-06 21:45:43 -0700

    sound effect + backgroun change

--- script/script.js
+++ script/script.js
@@ -2,7 +2,13 @@ let clickBox = document.getElementById("clickHere");
 let displayScore = document.getElementById("scoreBox");
 let score = 0;
 
+function playSound(sound) {
+	let soundPlayed = new Audio(sound);
+	soundPlayed.play();
+}
+
 clickBox.addEventListener("click", () => {
 	score += 1;
 	displayScore.innerText = score;
+	playSound("/sound/bonk.mp3");
 });
